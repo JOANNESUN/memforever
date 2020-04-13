29b86628461291ebe30cdf7da123d9d13e96ab5a
joannecysun@gmail.com <JOANNECYSUN@GMAIL.COM>
2020-04-14 01:14:45 +1000

    3rd commit frontend and backend connected

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ app.use(parser.urlencoded());
 app.set('view engine', 'ejs'); // use ejs view
 
 
-// app.use(express.static(__dirname+'/views/css'));
+app.use(express.static(__dirname+'/views'));
 
 
 
 
 app.get('/', function (req, res){
-res.send("home page");
+res.render('index');
 }
 ); // app is sever, / is route, function is how we should respond
 
@@ -23,13 +23,18 @@ app.get('/tracking', function (req, res){
     res.send("tracking in general");
     }
     );
-app.get('/game', function (req, res){
-        res.send("play game here");
+app.get('/daily', function (req, res){
+        res.render('daily');
         }
         );
+app.get('/game', function (req, res){
+        res.render('game');
+        }
+        );
+
         
 app.get('/user', function (req, res){
     res.send("user log in + data");
     }
     );
-app.listen('3003');
+app.listen('3005');
