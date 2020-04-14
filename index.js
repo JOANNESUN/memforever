71999e3cbbac8370f1d9267fd2edc998ff9e0cc6
joannecysun@gmail.com <JOANNECYSUN@GMAIL.COM>
2020-04-15 01:45:58 +1000

    backend working

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set('view engine', 'ejs'); // use ejs view
 
 
 app.use(express.static(__dirname+'/views'));
-
+// app.use(express.static(__dirname+'/views/script'));
 
 
 
@@ -27,11 +27,18 @@ app.get('/daily', function (req, res){
         res.render('daily');
         }
         );
+app.get('/daily/add', function (req, res){
+        res.render('addDaily');
+        }
+        );   
 app.get('/game', function (req, res){
         res.render('game');
         }
         );
-
+app.get('/map', function (req, res){
+        res.render('map');
+        }
+        );
         
 app.get('/user', function (req, res){
     res.send("user log in + data");
